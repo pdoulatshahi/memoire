8ea28d15223672c365a982f698fc3d15fb15bd9d
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-07-13 16:24:38 -0400

    added click functions, checking for matching cards

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-var foodVocab = ["la pomme", "la banane", "l'orange", "les raisins", "l'avocat", "le citron", "le citron vert", "la pastèque", "la mangue", "la poire", "la framboise", "la fraise", "la myrtille", "la cerise", "l'ananas" , "le pamplemousse"]
+var foodVocab = ["la pomme", "la banane", "une orange", "les raisins", "un avocat", "le citron", "le citron vert", "la pastèque", "la mangue", "la poire", "la framboise", "la fraise", "la myrtille", "la cerise", "un ananas", "le pamplemousse"]
 
 var foodImages = ["vocab/food/images/apple.jpeg", "vocab/food/images/banana.jpg", "vocab/food/images/orange.png", "vocab/food/images/grapes.jpg", "vocab/food/images/avocado.jpg", "vocab/food/images/lemon.png", "vocab/food/images/lime.png", "vocab/food/images/watermelon.jpg", "vocab/food/images/mango.png", "vocab/food/images/pear.png", "vocab/food/images/raspberry.png", "vocab/food/images/strawberry.png", "vocab/food/images/blueberry.gif", "vocab/food/images/cherry.png", "vocab/food/images/pineapple.jpeg", "vocab/food/images/grapefruit.png"]
-
-var tilesFlipped = 0;
-var clickCounter = 0;
-var output = [];
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -17,25 +13,62 @@ function shuffleArray(array) {
 }
 
 function newGame(){
-    var tilesFlipped = 0;
-    var food = foodVocab.concat(foodImages);
+    clickCounter = 0;
+    output = [];
+    clickedTiles = [];
+    tileIds = [];
+    tilesFlipped = 0;
+    food = foodVocab.concat(foodImages);
     shuffleArray(food);
     for(var i = 0; i < food.length; i++) {
-		output += '<div id="tile_'+i+'" onclick="clickOnTile(this,\''+food[i]+'\')"></div>';
+		output += '<div id="tile_'+i+'" onclick="clickOnTile(this,\''+food[i]+'\')"></div>'
 	  }
 	  document.getElementById('memory_board').innerHTML = output;
 }
 
 function clickOnTile(tile,val){
-    clickCounter++;
-    tile.style.background = '#DDD';
-		tile.innerHTML = val;
-    if (clickCounter % 2 === 0) {
-      if (foodVocab.indexOf(val) === foodImages.indexOf(val)){
-        console.log("test")
+    this.clickCounter++;
+    tile.style.background = 'green';
+    if (val.substring(0, 5) == "vocab"){
+      var tileImage = document.createElement("img");
+      tileImage.src = val;
+      tile.appendChild(tileImage);
+      clickedTiles.push(val);
+      tileIds.push(tile.id);
+    }
+    else if (val.substring(0, 5) != "vocab"){
+      tile.innerHTML = val;
+      clickedTiles.push(val);
+      tileIds.push(tile.id);
+    }
+    if (clickCounter === 2) {
+      if (((foodVocab.indexOf(clickedTiles[0]) === foodImages.indexOf(clickedTiles[1])) && (foodVocab.indexOf(clickedTiles[0]) !== -1))
+      || (foodVocab.indexOf(clickedTiles[1]) === foodImages.indexOf(clickedTiles[0])) && ((foodVocab.indexOf(clickedTiles[1])) !== -1)) {
+      tilesFlipped +=2;
+      clickedTiles = [];
+      clickCounter = 0;
+      $("#"+tileIds[0]).css("background", "blue");
+      $("#"+tileIds[1]).css("background", "blue");
+      }
+      else {
+      clickedTiles = [];
+      clickCounter = 0;
+        if ($("#"+tileIds[0]).find('img').length) {
+        $("#"+tileIds[0]).remove()
+        }
+      else {
+        ($("#"+tileIds[0]).text(""))
+        }
+      if ($("#"+tileIds[1]).find('img').length) {
+        $("#"+tileIds[1]).remove()
+        }
+      else {
+        ($("#"+tileIds[1]).text(""))
+        }
+      tileIds = [];
       }
     }
-}
+  }
 
 
-newGame();
+newGame()
---
+++ vocablist.md
@@ -0,0 +1,20 @@
+Fruits
+
+1. la pomme x
+2. la banane x
+3. l'orange x
+4. les raisins x
+5. l'avocat x
+6. le citron x
+7. le citron vert x
+8. la pastèque x
+9. la mangue x
+10. la poire x
+11. la framboise x
+12. la fraise x
+13. la myrtille x
+14. la cerise x
+15. l'ananas x
+16. le pamplemousse x
+
+Vegetables
